d02bcc9fc154e4bbe9cf5c61f46848f45f4c6287
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-17 21:06:27 +0300

    created photo obj

--- landing_krim/src/EachDay/EachDay.jsx
+++ landing_krim/src/EachDay/EachDay.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import classes from './EachDay.module.css';
 import { NavLink } from "react-router-dom";
+import image1 from './day1.JPG';
+import image2 from './day2.JPG';
+
+const backImages = {
+    '1': image1,
+    '2': image2
+}
 
 const EachDay = (props) => {
     return (
         <div className={classes.eachDay}>
-            <NavLink to={'/day/' + props.day}>День {props.day}</NavLink>
+            <img src={backImages[props.day]} alt=''></img>
+            <div className={classes.backImage}>
+                <NavLink to={'/day/' + props.day}>
+                    День {props.day}
+                </NavLink>
+            </div>
         </div>
     )
 }
