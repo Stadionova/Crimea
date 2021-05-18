2abe4513dd9fcbe0330eeba051e743bbcb6b7f82
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-18 21:29:04 +0300

    deleted Contacts component & added social icons to main page in header

--- landing_krim/src/App.jsx
+++ landing_krim/src/App.jsx
@@ -3,7 +3,6 @@ import classes from './App.module.css';
 import Trip from './Trip/Trip';
 import { Route } from "react-router-dom";
 import Equipment from './Equipment/Equipment';
-import Contacts from './Contacts/Contacts';
 import EachDay from './EachDay/EachDay';
 import React from "react";
 
@@ -13,7 +12,6 @@ function App() {
       <Route path='/main' component={MainPage} />
       <Route path='/trip' component={Trip} />
       <Route path='/equipment' component={Equipment} />
-      <Route path='/contacts' component={Contacts} />
       <Route path='/day' component={EachDay} />
     </div>
   );
--- landing_krim/src/Navigation/Navigation.jsx
+++ landing_krim/src/Navigation/Navigation.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import './Navigation.css';
+import './Navigation.module.css';
 import { NavLink } from "react-router-dom";
+import classes from './Navigation.module.css';
+import inst from './inst.png';
+import vk from './vk.png';
 
 const Navigation = (props) => {
     return (
-        <div className='navigation' data={props.changeColor}>
-            <div className='headerLinks'>
-                <NavLink to='/main' >Главная страница</NavLink>
-                <NavLink to='/trip' >Путешествие</NavLink>
-                <NavLink to='/equipment' >Снаряжение</NavLink>
-                <NavLink to='/contacts' >Контакты</NavLink>
+        <div className={classes.navigation} data={props.changeColor}>
+            <div className={classes.headerLinks}>
+                <NavLink to='/main'>Главная страница</NavLink>
+                <NavLink to='/trip'>Путешествие</NavLink>
+                <NavLink to='/equipment'>Снаряжение</NavLink>
+            </div>
+            <div className={classes.contacts}>
+                <a href="https://instagram.com/stadionova/">
+                    <img
+                        className={classes.inst}
+                        alt="Instagram"
+                        src={inst}>
+                    </img>
+                </a>
+                <a href="https://vk.com/tr.julia">
+                    <img
+                        className={classes.vk}
+                        alt="Vkontakte"
+                        src={vk}>
+                    </img>
+                </a>
             </div>
         </div>
     )
