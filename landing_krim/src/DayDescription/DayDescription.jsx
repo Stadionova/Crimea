f093eb8e63c65df4b0848814f74703c8ebe29839
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-19 22:25:44 +0300

    added data text

--- landing_krim/src/DayDescription/DayDescription.jsx
+++ landing_krim/src/DayDescription/DayDescription.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './DayDescription.module.css';
 import Navigation from '../Navigation/Navigation';
+import dataDaysDesc from '../dataDaysDesc';
 
 function DayDescription(props) {
     const data = props && props.location && props.location;
@@ -13,6 +14,9 @@ function DayDescription(props) {
             </div>
             <div>
                 <p>{data.aboutProps}</p>
+            </div>
+            <div>
+                <p>{dataDaysDesc[data.day]}</p>
             </div>
         </div>
     )
