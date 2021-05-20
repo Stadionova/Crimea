e50efe11dca8f804a400e33f32fd09dd29485cb5
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-20 12:49:38 +0300

    added little photos for 1 day

--- landing_krim/src/DayDescription/DayDescription.jsx
+++ landing_krim/src/DayDescription/DayDescription.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import classes from './DayDescription.module.css';
 import Navigation from '../Navigation/Navigation';
 import dataDaysDesc from '../dataDaysDesc';
+import image1 from './photos/day1/day1_1.JPG';
+import image2 from './photos/day1/day1_2.JPG';
+import image3 from './photos/day1/day1_3.JPG';
+import image4 from './photos/day1/day1_4.JPG';
+import image5 from './photos/day1/day1_5.JPG';
+import image6 from './photos/day1/day1_6.JPG';
 
 function DayDescription(props) {
     const data = props && props.location && props.location;
@@ -17,6 +23,23 @@ function DayDescription(props) {
             </div>
             <div>
                 <p>{dataDaysDesc[data.day]}</p>
+            </div>
+            <div className={classes.photoGallery}>
+                <ul>
+                    <div>
+                        <li><img src={image1} alt=''></img></li>
+                        <li><img src={image2} alt=''></img></li>
+                        <li><img src={image3} alt=''></img></li>
+                    </div>
+                    <div>
+                        <li><img src={image4} alt=''></img></li>
+                        <li><img src={image5} alt=''></img></li>
+                        <li><img src={image6} alt=''></img></li>
+                    </div>
+                </ul>
+            </div>
+            <div>
+                <p>ФОТО СЛАЙДЕР</p>
             </div>
         </div>
     )
