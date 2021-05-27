cefafc09ccc0d24cf6869688863970b31abbd82f
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-27 19:02:30 +0300

    styled Cities Component

--- landing_krim/src/ByCities/ByCities.jsx
+++ landing_krim/src/ByCities/ByCities.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import classes from './ByCities.module.css';
+import city1 from './city1.JPG';
+import city2 from './city2.JPG';
+import city3 from './city3.JPG';
 
 class ByCities extends React.Component {
     onclick() {
@@ -11,9 +14,18 @@ class ByCities extends React.Component {
             <div className={classes.description}>
                 <Navigation changeColor='black' />
                 <div className={classes.citiesWrapper}>
-                    <div className={classes.cities}>БАХЧИСАРАЙ</div>
-                    <div className={classes.cities}>СЕВАСТОПОЛЬ</div>
-                    <div className={classes.cities}>СИМФЕРОПОЛЬ</div>
+                    <div className={classes.cities}>
+                        <p>БАХЧИСАРАЙ</p>
+                        <img src={city1} alt=''></img>
+                    </div>
+                    <div className={classes.cities}>
+                        <p>СЕВАСТОПОЛЬ</p>
+                        <img src={city2} alt=''></img>
+                    </div>
+                    <div className={classes.cities}>
+                        <p>СИМФЕРОПОЛЬ</p>
+                        <img src={city3} alt=''></img>
+                    </div>
                 </div>
             </div>
         );
