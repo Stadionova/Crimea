8dca8214cb7e385021b3d06f31250229251896f8
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-29 18:06:04 +0300

    added Slider for Cities component & styled it

--- landing_krim/src/City/City.jsx
+++ landing_krim/src/City/City.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import classes from './City.module.css';
 import Navigation from '../Navigation/Navigation';
-import city1_1 from './city1_1.JPG';
-import city1_2 from './city1_2.JPG';
-import city2_1 from './city2_1.JPG';
-import city2_2 from './city2_2.JPG';
-import city3_1 from './city3_1.JPG';
-import city3_2 from './city3_2.JPG';
+
+import city1_1 from './city1/city1_1.JPG';
+import city1_2 from './city1/city1_2.JPG';
+import city1_3 from './city1/city1_3.JPG';
+import city1_4 from './city1/city1_4.JPG';
+import city1_5 from './city1/city1_5.JPG';
+import city1_6 from './city1/city1_6.JPG';
+import city1_7 from './city1/city1_7.JPG';
+import city1_8 from './city1/city1_8.JPG';
+import city1_9 from './city1/city1_9.JPG';
+
+import city2_1 from './city2/city2_1.JPG';
+import city2_2 from './city2/city2_2.JPG';
+import city2_3 from './city2/city2_3.JPG';
+import city2_4 from './city2/city2_4.JPG';
+import city2_5 from './city2/city2_5.JPG';
+import city2_6 from './city2/city2_6.JPG';
+import city2_7 from './city2/city2_7.JPG';
+import city2_8 from './city2/city2_8.JPG';
+import city2_9 from './city2/city2_9.JPG';
+
+import city3_1 from './city3/city3_1.JPG';
+import city3_2 from './city3/city3_2.JPG';
+import city3_3 from './city3/city3_3.JPG';
+import city3_4 from './city3/city3_4.JPG';
+import city3_5 from './city3/city3_5.JPG';
+import city3_6 from './city3/city3_6.JPG';
+import city3_7 from './city3/city3_7.JPG';
+import city3_8 from './city3/city3_8.JPG';
+import city3_9 from './city3/city3_9.JPG';
+
+import SwiperTextCities from '../SwiperTextCities/SwiperTextCities';
 
 const smallImages = {
-    '1': [city1_1, city1_2],
-    '2': [city2_1, city2_2],
-    '3': [city3_1, city3_2]
+    '1': [city1_1, city1_2, city1_3, city1_4, city1_5, city1_6, city1_7, city1_8, city1_9],
+    '2': [city2_1, city2_2, city2_3, city2_4, city2_5, city2_6, city2_7, city2_8, city2_9],
+    '3': [city3_1, city3_2, city3_3, city3_4, city3_5, city3_6, city3_7, city3_8, city3_9]
 }
 
 class City extends React.Component {
@@ -26,14 +52,7 @@ class City extends React.Component {
                 <div className={classes.eachDay}>
                     <img src={data.bigPhoto} alt=''></img>
                 </div>
-                <div className={classes.cityDayDesc}>
-                    <img src={smallImages[data.cityNum][0]} alt=''></img>
-                    <p>{data.desc[0]}</p>
-                </div>
-                <div className={classes.cityDayDesc}>
-                    <img src={smallImages[data.cityNum][1]} alt=''></img>
-                    <p>{data.desc[1]}</p>
-                </div>
+                <SwiperTextCities sliderPhotos={smallImages[data.cityNum]} />
                 <p className={classes.footer}>© 2021 Мама и Дети, Крым</p>
             </div>
         );
