1f6adcf27141d446bd2027f09d4f1491fc99f216
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-06-01 20:28:39 +0300

    updates

--- landing_krim/src/index.js
+++ landing_krim/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop/ScrollToTop';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>, document.getElementById('root')
+  </HashRouter>, document.getElementById('root')
 );
 
 reportWebVitals();
